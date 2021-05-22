fa361b3a0bacc73e6cc4aafb27eabfee8e15b309
modo <modobit@users.noreply.github.com>
2021-05-22 10:31:11 -0400

    update

--- Basic App/app.js
+++ Basic App/app.js
@@ -2,11 +2,8 @@
 
      data() {
          return {
+             heading: 'Welcome to vue tutorial',
 
-            boxASelected: false,
-            boxBSelected: false,
-            boxCSelected: false,
-            heading: 'Welcome to vue tutorial',
              link_google: 'https://google.com/',
              link_yahoo: 'https://yahoo.com',
              counter: 0,
@@ -16,21 +13,6 @@
      },
 
      methods: {
-
-
-        boxSelected(box) {
-            if (box === 'A') {
-              this.boxASelected = !this.boxASelected;
-              console.log("Selected A")
-            } else if (box === 'B') {
-              this.boxBSelected = !this.boxBSelected;
-              console.log("Selected B")
-            } else if (box === 'C') {
-              this.boxCSelected = !this.boxCSelected;
-              console.log("Selected C")
-            }
-          },
-        
 
          setName(event) {
              this.name = event.target.value;
